a2c3fe71954cc564c2fb4f2d04ccc8ffece92fb2
harshalshah7 <harshal13061994@gmail.com>
2017-02-05 18:49:59 -0700

    Update file1.py

--- Graphs1/file1.py
+++ Graphs1/file1.py
@@ -1,13 +1,15 @@
-#author: Harshal Shah Harshal.Shah@colorado.edu
-#name: file1.py
-#purpose: Create a line with random data
-#date: 10/26/2016
-#version: 8.1
-  
-  
+###############################################################################################################
+# Copyrights 2016 Harshal Shah All Rights Reserved
+# The information contained herein is property of the Authors.
+# The copying and distribution of the files is prohibited except by express written agreement with the Authors.
+# Authors: Harshal Shah
+#Date: Aug 2016
+#purpose: Create a line with random data  
+###############################################################################################################
 import sys
 import random
 import matplotlib.pyplot as plt
+
 y=[]
 for i in range (0, 25):
     x= random.randint(10,100) 
@@ -15,8 +17,7 @@ for i in range (0, 25):
 plt.plot(y,color='black')
 plt.xlabel("Other numbers")
 plt.ylabel("Random Numbers")
-plt.title("My first graph")
-    
+plt.title("My first graph")   
 print(y)
 plt.show()
 sys.exit()
