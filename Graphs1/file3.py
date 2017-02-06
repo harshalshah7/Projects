9910109b206ac361d697c5a5e1d2fe705fb12fcb
harshalshah7 <harshal13061994@gmail.com>
2017-02-05 18:53:47 -0700

    Update file3.py

--- Graphs1/file3.py
+++ Graphs1/file3.py
@@ -1,22 +1,23 @@
-#author: Harshal Shah Harshal.Shah@colorado.edu
-#name: file3.py
+##############################################################################################################
+# Copyrights 2016 Harshal Shah All Rights Reserved
+# The information contained herein is property of the Authors.
+# The copying and distribution of the files is prohibited except by express written agreement with the Authors.
+# Authors: Harshal Shah
 #purpose: Create  pie chart of the cartheft.csv file
 #date: 10/26/2016
-#version: 8.3
+###############################################################################################################
 import sys
 import os
 import csv
-
 import matplotlib.pyplot as plt
+
 compname=[]
 data=[]
 if os.path.isfile("cartheft.csv"):
-    
     with open('cartheft.csv') as csvfile:
         reader=csv.reader(csvfile)
         for row in reader:
             if row:
-                
                 if float(row[4]) > 2.5 :
                     data.append(row[4])
                     compname.append(row[0]+ "-" + row[1]) 
@@ -26,7 +27,6 @@ if os.path.isfile("cartheft.csv"):
         print("1")
         plt.title("Pie Chart")
         plt.pie(perc,colors=colors, labels=labels, autopct = '%1.1f%%', shadow=True, startangle=90)
-    
         plt.axis('equal')
         plt.show()
 else:
